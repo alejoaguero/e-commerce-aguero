a9e4e06195eb12be694437cc24a3f542986bcb7d
Alejo <alejoleonel45@gmail.com>
2022-02-01 01:20:44 -0300

    Contador de Productos arreglado y limitado con el stock

--- src/Components/ItemList/Item.js
+++ src/Components/ItemList/Item.js
@@ -1,22 +1,38 @@
-import react, { useEffect } from "react";
+import react, { useEffect, useState } from "react";
 
 function Item({name,description,stock,setItemSelected}){
 const cambiarNombre = () =>{
     setItemSelected({name,description,stock})
 }
+const [contador,setContadorValue] = useState(0)
+
+const  sumContador = () =>{
+        if(contador===stock){
+            return setContadorValue(contador)
+        }
+            else{
+                return setContadorValue(contador+1)
+            }
+}
+
+const minContador = () =>{
+    if(contador>0){
+    return setContadorValue(contador-1)
+    }
+}
 
 
 
-    
+
     return(
         <div className="articulos__individuales">
-            <h3>{name}</h3>
+            <h3>Producto:{name}</h3>
             <p>{description}</p>
-            <h6>{stock}</h6>
-                <div>
-                    <button>+</button>
-                    <input />
-                    <button>-</button>
+            <h6>Stock: {stock}</h6>
+                <div className="contadorStock">
+                    <button className="contadorStock__mas" onClick={sumContador}>+</button>
+                    <span>{contador}</span>
+                    <button className="contadorStock__menos" onClick={minContador}>-</button>
                 </div>
             <button onClick={cambiarNombre}>Agregar</button>
         </div>
