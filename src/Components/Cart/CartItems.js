dacc8b8421b8b392327b6f54c423a2a669dbbaf1
Alejo <alejoleonel45@gmail.com>
2022-02-21 19:24:38 -0300

    Creacion del Carrito con context

--- src/Components/Cart/CartItems.js
+++ src/Components/Cart/CartItems.js
@@ -1,11 +1,15 @@
 import React, { useContext } from 'react'
 import { CartProvider } from '../../context/CartContext'
+import '../CSS/ItemCart.css'
 
-const CartItems = ({item}) => {
-const {cart,removeItem} = useContext(CartProvider)
+const CartItems = ({item,quantity}) => {
+const {removeItem} = useContext(CartProvider)
 
   return (
-    <div>
+    <div className='containerCart'>
+        <h4>{item.name}</h4>
+        <img className='imageCart' src={item.imagen} alt={item.name}></img>
+        <h5>Cantidad: {quantity} - Total: ${item.precio*quantity}</h5>
         <button onClick={() => removeItem(item.id)}>-</button>
     </div>
   )
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -30,7 +30,7 @@ const clearItems = () => {
 }
   
 const isInCart = Boolean;
-
+  
 
 
 
